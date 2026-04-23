cc942be6f2da32da927ee8d00e86b22f1734acef
Aman-kumar-nitm <amankumarec2023nitm@gmail.com>
2026-04-23 13:28:51 +0530

    deployment

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
-
+import pickle
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import RandomizedSearchCV
 def save_object(file_path,obj):
@@ -47,5 +47,15 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models,params):
             trained_models[model_name] = best_model
 
         return report, trained_models
+    
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+        
